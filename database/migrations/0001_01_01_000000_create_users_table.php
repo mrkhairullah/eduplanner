e29d68baa27558510f2cc33310ab41071b2f566b
Muhammad Rizky Khairullah <mrkhairullah.id@gmail.com>
2026-03-20 23:49:15 +0700

    feat: add additional fields to several tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,27 +13,52 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name', 64);
+            $table->string('username', 32)
+                ->unique();
+            $table->string('email', 64)
+                ->nullable()
+                ->unique();
+            $table->unsignedTinyInteger('gender')
+                ->nullable();
+            $table->unsignedTinyInteger('religion')
+                ->nullable();
+            $table->string('birth_place', 64)
+                ->nullable();
+            $table->date('birth_date')
+                ->nullable();
+            $table->unsignedTinyInteger('entry_month')
+                ->nullable();
+            $table->year('entry_year')
+                ->nullable();
+            $table->timestamp('email_verified_at')
+                ->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email')
+                ->primary();
             $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->timestamp('created_at')
+                ->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
+            $table->string('id')
+                ->primary();
+            $table->foreignId('user_id')
+                ->nullable()
+                ->index();
+            $table->string('ip_address', 45)
+                ->nullable();
+            $table->text('user_agent')
+                ->nullable();
             $table->longText('payload');
-            $table->integer('last_activity')->index();
+            $table->integer('last_activity')
+                ->index();
         });
     }
 
--- database/migrations/2026_03_20_164025_create_permission_tables.php
+++ database/migrations/2026_03_20_164025_create_permission_tables.php
@@ -43,6 +43,8 @@ return new class extends Migration
             }
             $table->string('name');
             $table->string('guard_name');
+            $table->boolean('login')
+                ->default(true);
             $table->timestamps();
             if ($teams || config('permission.testing')) {
                 $table->unique([$columnNames['team_foreign_key'], 'name', 'guard_name']);
